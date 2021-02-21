86f199fcca47b6dcaea4f65097e4d9079250a22f
MiriErnfeld <miri@leader.codes>
2021-02-21 15:29:59 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,29 +4,16 @@ import store from './Redux/staticStore'
 import InformationTemp from './components/InformationTemp'
 import Chart2 from './components/Chart2'
 import FusionChart from './components/FusionChart'
-import AreaChart from './components/AreaChart'
-
-
 
 function App() {
   return (
     <div className="container-fluid ">
       <Provider store={store}>
-
         <InformationTemp></InformationTemp>
         <Chart2></Chart2>
         <FusionChart></FusionChart>
-    
-       
-        {/* <Fasion2></Fasion2>
-        <TotalChart></TotalChart> */}
-       
-
       </Provider>
-
     </div>
-
-
   );
 }
 export default App;
