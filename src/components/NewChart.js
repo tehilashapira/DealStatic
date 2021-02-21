5f1177b4309f78bb2bb4f05988f0f2c7d75f6d56
MiriErnfeld <miri@leader.codes>
2021-02-21 16:30:41 +0200

    --- Auto-Git Commit ---

--- src/components/NewChart.js
+++ src/components/NewChart.js
@@ -1,5 +1,5 @@
 import React, { useRef, useLayoutEffect } from 'react';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
@@ -8,54 +8,56 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
 
 export default function App(props) {
-const UpdatedData = useSelector(state => state.staticDetailsReducer);
+    const UpdatedData = useSelector(state => state.staticDetailsReducer);
 
-  const chart = useRef(null);
+    const chart = useRef(null);
 
-  useLayoutEffect(() => {
-    let x = am4core.create("chartdiv", am4charts.XYChart);
+    useLayoutEffect(() => {
+        let x = am4core.create("chartdiv", am4charts.XYChart);
 
-    x.paddingRight = 20;
-   let data= [
-        UpdatedData.leaderStatic.sumContacts,
-        UpdatedData.leaderStatic.sumPapers,
-        UpdatedData.leaderStatic.sumProjects,
-        UpdatedData.leaderStatic.sumTasks,
-    ]
-    let visits = 10;
+        x.paddingRight = 20;
+        //    let data= [
+        //         UpdatedData.leaderStatic.sumContacts,
+        //         UpdatedData.leaderStatic.sumPapers,
+        //         UpdatedData.leaderStatic.sumProjects,
+        //         UpdatedData.leaderStatic.sumTasks,
+        //     ]
+        let data = []
+        let visits = 10;
 
-    // for (let i = 1; i < 366; i++) {
-    //   visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-    //   data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-    // }
+        for (let i = 1; i < 366; i++) {
+            visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+            data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+        }
 
-    x.data = data;
+        x.data = data;
 
-    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
+        let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+        // let dateAxis =["contact","papers","projects","tasks"]
 
-    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    valueAxis.renderer.minWidth = 35;
+        let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
 
-    let series = x.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
-    series.tooltipText = "{valueY.value}";
-    x.cursor = new am4charts.XYCursor();
+        let series = x.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+        series.tooltipText = "{valueY.value}";
+        x.cursor = new am4charts.XYCursor();
 
-    let scrollbarX = new am4charts.XYChartScrollbar();
-    scrollbarX.series.push(series);
-    x.scrollbarX = scrollbarX;
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        x.scrollbarX = scrollbarX;
 
-    chart.current = x;
+        chart.current = x;
 
-    return () => {
-      x.dispose();
-    };
-  }, []);
+        return () => {
+            x.dispose();
+        };
+    }, []);
 
-  return (
-    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-  );
+    return (
+        <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+    );
 }
