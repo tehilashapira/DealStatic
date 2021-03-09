694e3de1fe1a9c6d6639f7a51a52d2b622dca0e5
MiriErnfeld <miri@leader.codes>
2021-03-09 20:38:31 +0200

    --- Auto-Git Commit ---

--- src/components/ChartT.js
+++ src/components/ChartT.js
@@ -77,7 +77,7 @@ export default function ApexChart() {
       },
       stroke: {
         curve: 'smooth',
-        width: 1,
+        width: 2,
       },
       xaxis: {
         categories: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
